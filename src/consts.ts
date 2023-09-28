17c2615296a95ae02dd630ae78fc703e2524850a
camilo ovalle <camilo.enrique.ovalle@gmail.com>
2023-09-28 00:06:52 -0500

    I updated the content of the main index of the page and removed the tutorials layout bucause it was not necesary, and finally, I changed the cover for the git and github blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'DEV🚀Gate';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const GITHUB = 'https://github.com/Camilo-ovalle';
+export const WHATSAPP = 'https://api.whatsapp.com/send?phone=573192966892';
